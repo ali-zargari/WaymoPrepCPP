29121550dcc6b64b67f14a92561c81ce73fc1c6f
aliza <alizarg21@gmail.com>
2025-04-15 18:11:11 -0700

    Completed

--- main.cpp
+++ main.cpp
@@ -1,30 +1,44 @@
 #include <iostream>
 #include <deque>
+#include <vector>
+#include <set>
+#include <unordered_set>
 
 using namespace std;
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-int main()
+
+class Solution
 {
-    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the
-    // <b>lang</b> variable name to see how CLion can help you rename it.
-    string lang = "C++";
 
-    cout << "Hello and welcome to " << lang << "!\n";
-
-    for (int i = 1; i <= 5; i++)
+public:
+    bool oldHasDuplicate(vector<int>& nums)
     {
-        // TIP Press <shortcut actionId="Debug"/> to start debugging your code.
-        // We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/>
-        // breakpoint for you, but you can always add more by pressing
-        // <shortcut actionId="ToggleLineBreakpoint"/>.
-        cout << "i = " << i << std::endl;
+        set<int> s;
+        int len = nums.size();
+
+        for (auto i : nums)
+        {
+            // cout << i << endl;
+            s.insert(i);
+        }
+
+         return !((len == s.size()));
     }
 
-    return 0;
-}
+    bool hasDuplicate(vector<int>& nums)
+    {
+        return unordered_set(nums.begin(), nums.end()).size() != nums.size();
+    }
+};
 
-// TIP See CLion help at <a
-// href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>.
-//  Also, you can try interactive lessons for CLion by selecting
-//  'Help | Learn IDE Features' from the main menu.
+
+int main()
+{
+    Solution* s = new Solution();
+    vector<int>* v= new vector<int>();
+    v->push_back(2);
+    v->push_back(2);
+    v->push_back(3);
+
+    cout << s->hasDuplicate(*v);
+
+}

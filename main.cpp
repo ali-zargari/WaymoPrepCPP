56983ee84785f8c6177b6a205513b67c847d625f
aliza <alizarg21@gmail.com>
2025-04-17 19:36:26 -0700

    Completed

--- main.cpp
+++ main.cpp
@@ -1,45 +1,50 @@
+#include <iomanip>
 #include <iostream>
 #include <vector>
+#include <stack>
+#include <algorithm>
+
 
 using namespace std;
 
 class Solution {
 public:
-    int trap(vector<int>& height) {
-        int left = 0;
-        int right = height.size()-1;
-        int water = 0;
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+        //We can simulate each "tick" using a forloop.
+        //If a car is overtaking another car, we set the speed of the faster one to the one it is overtaking.
+        //Every time a car pos gets to the target, we add one to the fleet.
+        //but this isnt optimal.
 
-        int left_wall=height[0], right_wall=height[height.size()-1];
+        //We can calculate the time it takes for each car to get to the target mathematically.
+        //We add these times to a monotonically increasing stack. the length of this stack is going to be the number of fleets.
 
-        while (left < right)
+
+        vector<pair<int, float>> cars;
+
+        for (int i = 0; i < position.size(); i++)
         {
-            if (height[left] < height[right])
-            {
-                if (left_wall<height[left])
-                {
-                    left_wall = height[left];
-                } else
-                {
-                    water += (left_wall-height[left]);
-                }
-                left++;
-            } else
-            {
-                if (right_wall<height[right])
-                {
-                    right_wall = height[right];
-                } else
-                {
-                    water += (right_wall-height[right]);
-                }
-                right--;
+            cars.push_back({position[i], (float)(target-position[i])/(float)speed[i]});
+
+        }
+
+        sort(cars.rbegin(), cars.rend());
+
+        stack<double> fleet_times; // Stack to store arrival times of fleet leaders
+
+
+        for (int i = 0; i < position.size(); ++i) {
+            double current_time = cars[i].second;
+
+            if (fleet_times.empty() || current_time > fleet_times.top()) {
+                fleet_times.push(current_time);
             }
         }
 
-        return water;
+        return fleet_times.size();
+
     }
 };
+
 
 
 int main()
@@ -47,9 +52,11 @@ int main()
 
     Solution s;
 
-    vector<int> top = {0,2,0,3,1,0,1,3,2,1};
+    vector<int> pos = {10,8,0,5,3};
+    vector<int> spd = {2,4,1,1,3};
+    int target = 12;
 
-    cout << s.trap(top) << endl;
+    cout << s.carFleet(target, pos, spd) << endl;
 
     return 0;
 }

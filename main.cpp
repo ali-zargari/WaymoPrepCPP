9543e726fc6f5722de162f1565fd0244c1a33ff9
aliza <alizarg21@gmail.com>
2025-04-28 15:01:07 -0700

    Completed

--- main.cpp
+++ main.cpp
@@ -1,65 +1,60 @@
-#include <cmath>
-#include <iostream>
-#include <map>
-#include <queue>
-#include <set>
-#include <unordered_map>
-
 #include <vector>
-
+#include <unordered_map>
+#include <queue>
+#include <iostream>
 
 using namespace std;
 
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
-        //We can go through the tasks. We process the first one. We keep going through the list. If the next is equal,
-        //we remove it (maybe add it to a waiting queue). Then continue, and check waiting queue. If one is ready,
-        //then we pop from queue, and process.
+        //
 
+        vector<int> freq(26, 0);
 
-        vector<int> count(26, 0);
-
-        for (const char i: tasks)
-        {
-            count[i-'A']++;
+        for(char task:tasks){
+            freq[task-'A']++;
         }
 
-        priority_queue<int> task_list;
-        for (int i : count)
-        {
-            if (i != 0)
-            {
-                task_list.push(i);
+        priority_queue<int> pq;
+
+        for(int i : freq){
+            if(i != 0){
+                pq.push(i);
             }
         }
 
-        int time = 0;
+        // count, cooldown
         queue<pair<int, int>> waiting;
 
-        while (!waiting.empty() || !task_list.empty())
-        {
+        int time = 0;
 
-            if (!waiting.empty() && waiting.front().second <= time)
-            {
-                task_list.push(waiting.front().first);
+
+        while(!waiting.empty() || !pq.empty()){
+
+
+            if(!waiting.empty() && waiting.front().second == time){
+                auto temp = waiting.front();
                 waiting.pop();
+
+                if(temp.first != 0)
+                    pq.emplace(temp.first);
             }
 
-            if (!task_list.empty())
-            {
-                int temp = task_list.top();
-                task_list.pop();
-                if (temp - 1 > 0)
-                {
-                    waiting.emplace(temp - 1, time + n + 1);
+            if(!pq.empty()){
+                auto temp = pq.top();
+                pq.pop();
+                if(temp-1 > 0)
+                    waiting.emplace(temp-1, time+n+1);
+            }
+
+
+            if(waiting.front().first == 0){
+                waiting.pop();
+                if(pq.empty() && waiting.empty()){
+                    return time;
                 }
-
-            } else
-            {
-                time = waiting.front().second;
             }
-
 
             time++;
         }
@@ -70,39 +65,27 @@ public:
 };
 
 
-// Main function provided by user - unchanged.
-int main()
-{
+
+// To execute C++, please define "int main()"
+int main() {
 
 
+    //A, A, A, B, C, B
     Solution s;
 
-    //s.leastInterval({'A', 'A', 'A', 'B', 'C', 'B'}, 2);
+    vector<char> v = {'X', 'X', 'Y', 'Y'};
+    vector<char> v2 = {'A', 'A', 'A', 'B', 'C'};
 
+    //A, A, A, B, C, B ; n=2
+    //A-B-C-A-B-Idle-A
 
-    vector<char> tasks1 = {'A', 'A', 'A', 'B', 'C', 'B'};
-    int n1 = 2;
-    cout << "Test Case 1: " << s.leastInterval(tasks1, n1) << endl;
+    //A, A, A, B, C ; n=3
+    //A-B-C-Idle-A-Idle-Idle-Idle-A
 
-    vector<char> tasks2 = {'A', 'A', 'A', 'B', 'B', 'B'};
-    int n2 = 0;
-    cout << "Test Case 2: " << s.leastInterval(tasks2, n2) << endl;
+    //3, 1, 1
 
-    vector<char> tasks3 = {'A', 'B', 'C', 'A', 'B', 'A'};
-    int n3 = 2;
-    cout << "Test Case 3: " << s.leastInterval(tasks3, n3) << endl;
-
-    vector<char> tasks4 = {'A'};
-    int n4 = 2;
-    cout << "Test Case 4: " << s.leastInterval(tasks4, n4) << endl;
-
-    vector<char> tasks5 = {'A', 'A', 'B', 'B', 'C', 'C', 'D', 'D'};
-    int n5 = 3;
-    cout << "Test Case 5: " << s.leastInterval(tasks5, n5) << endl;
-
-    //["A","A","A","B","C","B"], 2
-
-    // A -> B -> C -> A -> B -> -> A
+    cout << s.leastInterval(v, 2) << endl;
+    cout << s.leastInterval(v2, 3) << endl;
 
     return 0;
 }

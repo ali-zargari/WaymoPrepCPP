b5465ea594116ff0bb3001463374346933a09660
aliza <alizarg21@gmail.com>
2025-04-28 19:38:59 -0700

    Completed

--- main.cpp
+++ main.cpp
@@ -6,44 +6,50 @@ using namespace std;
 
 class Solution {
 public:
-    int numIslands(vector<vector<char>>& grid) {
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+
 
         if (grid.size() == 0)
         {
             return 0;
         }
 
+
+        int area = 0;
         vector<vector<bool>> visited(grid.size(), vector<bool>(grid[0].size(), false));
 
-        int island_count = 0;
 
-        auto dfs = [&grid, &visited](int x, int y)
+        auto dfs = [&](int x, int y)
         {
-            visited[x][y] = true;
-            int dir [4][2]  = {{1, 0},{-1, 0},{0, 1},{0, -1}};
+            int temp_area = 1;
+            int dir[4][2] = {{1, 0},{-1, 0},{0, 1},{0, -1}};
             stack<pair<int, int>> stk;
+
             stk.emplace(x, y);
+
+            visited[x][y] = true;
 
             while (!stk.empty())
             {
 
-                pair<int, int> node = stk.top();
+                pair <int, int> node = stk.top();
                 stk.pop();
 
-                for (auto d: dir)
+                for (int* d :dir)
                 {
-                    if ((node.first + d[0] >= 0 && node.first + d[0] < grid.size()) &&
-                        (node.second + d[1] >= 0 && node.second + d[1] < grid[0].size())) {
-
-                        if (!visited[node.first + d[0]][node.second + d[1]] &&
-                            grid[node.first + d[0]][node.second + d[1]] == '1')
-                        {
-                            visited[node.first + d[0]][node.second + d[1]] = true;
-                            stk.emplace(node.first + d[0], node.second + d[1]);
-                        }
+                    if ((node.first + d[0] < grid.size() && node.first + d[0] >= 0) &&
+                        (node.second + d[1] < grid[0].size() && node.second + d[1] >= 0) &&
+                        !visited[node.first + d[0]][node.second + d[1]] &&
+                        grid[node.first + d[0]][node.second + d[1]] == 1)
+                    {
+                        stk.emplace(node.first + d[0], node.second + d[1]);
+                        temp_area += 1;
+                        visited[node.first + d[0]][node.second + d[1]] = true;
                     }
                 }
             }
+
+            area = max(area, temp_area);
         };
 
 
@@ -51,19 +57,17 @@ public:
         {
             for (int j = 0; j < grid[i].size(); j++)
             {
-                //cout << grid[i][j] << " " << boolalpha<< (grid[i][j] == 1 && !visited[i][j]) ;
-                if (grid[i][j] == '1' && !visited[i][j])
+                if (grid[i][j] == 1 && !visited[i][j])
                 {
                     dfs(i, j);
-                    island_count++;
                 }
             }
-            //cout << endl;
         }
 
-        return island_count;
+        return area;
     }
 };
+
 
 
 // To execute C++, please define "int main()"
@@ -73,49 +77,49 @@ int main() {
 
 
     // Test case 1: Basic 4x5 grid with 2 islands
-    vector<vector<char>> grid1 = {
-        {'1', '1', '0', '0', '0'},
-        {'1', '1', '0', '0', '0'},
-        {'0', '0', '1', '0', '0'},
-        {'0', '0', '0', '1', '1'}
+    vector<vector<int>> grid1 = {
+        {1, 1, 0, 0, 0},
+        {1, 1, 0, 0, 0},
+        {0, 0, 1, 0, 0},
+        {0, 0, 0, 1, 1}
     };
-    cout << "Number of islands in grid1: " << s.numIslands(grid1) << endl; // Expected: 3
+    cout << "Expected: 4, Actual: " << s.maxAreaOfIsland(grid1) << endl;
 
     // Test case 2: Grid with no land ('1')
-    vector<vector<char>> grid2 = {
-        {'0', '0', '0', '0', '0'},
-        {'0', '0', '0', '0', '0'},
-        {'0', '0', '0', '0', '0'}
+    vector<vector<int>> grid2 = {
+        {0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0}
     };
-    cout << "Number of islands in grid2: " << s.numIslands(grid2) << endl; // Expected: 0
+    cout << "Expected: 0, Actual: " << s.maxAreaOfIsland(grid2) << endl;
 
     // Test case 3: Grid with all land ('1')
-    vector<vector<char>> grid3 = {
-        {'1', '1', '1'},
-        {'1', '1', '1'},
-        {'1', '1', '1'}
+    vector<vector<int>> grid3 = {
+        {1, 1, 1},
+        {1, 1, 1},
+        {1, 1, 1}
     };
-    cout << "Number of islands in grid3: " << s.numIslands(grid3) << endl; // Expected: 1
+    cout << "Expected: 9, Actual: " << s.maxAreaOfIsland(grid3) << endl;
 
     // Test case 4: Grid with one vertical island
-    vector<vector<char>> grid4 = {
-        {'1', '0', '0'},
-        {'1', '0', '0'},
-        {'1', '0', '0'}
+    vector<vector<int>> grid4 = {
+        {1, 0, 0},
+        {1, 0, 0},
+        {1, 0, 0}
     };
-    cout << "Number of islands in grid4: " << s.numIslands(grid4) << endl; // Expected: 1
+    cout << "Expected: 3, Actual: " << s.maxAreaOfIsland(grid4) << endl;
 
     // Test case 5: Grid with one horizontal island
-    vector<vector<char>> grid5 = {
-        {'0', '0', '0', '0'},
-        {'1', '1', '1', '1'},
-        {'0', '0', '0', '0'}
+    vector<vector<int>> grid5 = {
+        {0, 0, 0, 0},
+        {1, 1, 1, 1},
+        {0, 0, 0, 0}
     };
-    cout << "Number of islands in grid5: " << s.numIslands(grid5) << endl; // Expected: 1
+    cout << "Expected: 4, Actual: " << s.maxAreaOfIsland(grid5) << endl;
 
     // Test case 6: Empty grid (edge case)
-    vector<vector<char>> grid6 = {};
-    cout << "Number of islands in grid6: " << s.numIslands(grid6) << endl; // Expected: 0 (if handled)
+    vector<vector<int>> grid6 = {};
+    cout << "Expected: 0, Actual: " << s.maxAreaOfIsland(grid6) << endl;
 
 
     return 0;

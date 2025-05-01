f01add88249330d1d2c36814479e500ca3f5b6d0
aliza <alizarg21@gmail.com>
2025-05-01 11:49:42 -0700

    Completed

--- main.cpp
+++ main.cpp
@@ -1,55 +1,90 @@
-#include <algorithm>
-#include <iostream>
+#include <limits.h>
 #include <map>
 #include <queue>
-#include <stack>
-#include <vector>
-#include <unordered_set>
-#include <unordered_map>
+#include <set>
+#include <iostream>
 
 
 using namespace std;
 
-class Solution
+class HitCounter
 {
 public:
 
-    vector<int> getToal(int N, vector<pair<int, int>> tickets)
+    vector<pair<int, int>> timeline;
+    int count;
+
+    HitCounter()
     {
-        // N timestamps: 0,1,…,N-1
-        vector<int> diff(N + 1, 0);
-
-        for (auto& t : tickets)
-        {
-            diff[t.first]++; // car arrives at time=in
-            if (t.second + 1 <= N) // remove it after time=out
-                diff[t.second + 1]--;
-        }
-
-        // build the prefix-sum to get counts at times 0..N-1
-        vector<int> ans(N);
-        int cur = 0;
-        for (int time = 0; time < N; ++time)
-        {
-            cur += diff[time];
-            ans[time] = cur;
-        }
-        return ans;
+        count = 0;
+        timeline = vector<pair<int, int>>(300, {0, 0});
     }
+
+    void hit(int timeStamp)
+    {
+        int index = timeStamp % 300;
+
+        if (timeline[index].first == timeStamp)
+        {
+            timeline[index].second++;
+        } else
+        {
+            timeline[index].first = timeStamp;
+            timeline[index].second = 1;
+        }
+
+    }
+
+    int getHits(int timeStamp)
+    {
+        int total = 0;
+
+        for (int i = 0; i < 300; i++)
+        {
+            pair<int, int> bucket = timeline[i];
+
+            if (timeStamp - bucket.first < 300)
+            {
+                total += bucket.second;
+            }
+        }
+
+        return total;
+    }
+
+
 };
 
+// hit(1) adds hit to timestamp 1
+// hit(2) adds hit to timestamp 2
+// hit(3) adds hit to timestamp 3
+// 1-2-3 ... 304
+// get hit(4) 3-300 = -297...1 2 3
 
 // --- Main function with test cases (modified slightly for clarity) ---
 int main()
 {
-    Solution s;
+    HitCounter counter;
 
-    // Test Case 3: Multiple cars with overlapping times
-    vector<pair<int, int>> test3 = {{1, 3}, {2, 4}};
-    vector<int> result3 = s.getToal(5, test3);
-    cout << "Test 1 (Multiple cars): ";
-    for (int x : result3) cout << x << " ";
-    cout << endl;
+// hit at timestamp 1.
+    counter.hit(1);
 
+    // hit at timestamp 2.
+    counter.hit(2);
+
+    // hit at timestamp 3.
+    counter.hit(3);
+
+    // get hits at timestamp 4, should return 3.
+    cout << counter.getHits(4) << endl;
+
+    // hit at timestamp 300.
+    counter.hit(300);
+
+    // get hits at timestamp 300, should return 4.
+    cout << counter.getHits(300) << endl;
+
+    // get hits at timestamp 301, should return 3.
+    cout << counter.getHits(301) << endl;
     return 0;
 }

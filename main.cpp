db355449e6d51f36c1b56abe289b1052d0fffbd5
aliza <alizarg21@gmail.com>
2025-05-03 16:23:59 -0700

    Completed

--- main.cpp
+++ main.cpp
@@ -1,67 +1,54 @@
 #include <vector>
 #include <iostream>
+#include <queue>
+#include <set>
 #include <stack>
+#include <unordered_map>
 
 using namespace std;
 
 class Solution {
 public:
-    int numIslands(vector<vector<char>>& grid) {
+    int numPart(int n, vector<vector<int>> dislikes) {
 
-        if (grid.size() == 0)
-        {
-            return 0;
+
+        vector<vector<int>> adj(n+1);
+        for (auto &e : dislikes) {
+            adj[e[0]].push_back(e[1]);
+            adj[e[1]].push_back(e[0]);
         }
 
-        vector<vector<bool>> visited(grid.size(), vector<bool>(grid[0].size(), false));
+        vector<int> color(n+1, 0);
 
-        int island_count = 0;
 
-        auto dfs = [&grid, &visited](int x, int y)
+        for (int i = 1; i <= n; i++)
         {
-            visited[x][y] = true;
-            int dir [4][2]  = {{1, 0},{-1, 0},{0, 1},{0, -1}};
-            stack<pair<int, int>> stk;
-            stk.emplace(x, y);
+            if (color[i] != 0) continue;
 
-            while (!stk.empty())
+            color[i] = 1;
+            queue<int> q;
+            q.push(i);
+
+            while (!q.empty())
             {
+                int curr = q.front();
+                q.pop();
 
-                pair<int, int> node = stk.top();
-                stk.pop();
-
-                for (auto d: dir)
+                for (int d :adj[curr])
                 {
-                    if ((node.first + d[0] >= 0 && node.first + d[0] < grid.size()) &&
-                        (node.second + d[1] >= 0 && node.second + d[1] < grid[0].size())) {
-
-                        if (!visited[node.first + d[0]][node.second + d[1]] &&
-                            grid[node.first + d[0]][node.second + d[1]] == '1')
-                        {
-                            visited[node.first + d[0]][node.second + d[1]] = true;
-                            stk.emplace(node.first + d[0], node.second + d[1]);
-                        }
+                    if (color[d] == 0)
+                    {
+                        q.push(d);
+                        color[d] = -1 * color[curr];
+                    } else if (color[d] == color[curr])
+                    {
+                        return false;
                     }
                 }
             }
-        };
-
-
-        for (int i = 0; i < grid.size(); i++)
-        {
-            for (int j = 0; j < grid[i].size(); j++)
-            {
-                //cout << grid[i][j] << " " << boolalpha<< (grid[i][j] == 1 && !visited[i][j]) ;
-                if (grid[i][j] == '1' && !visited[i][j])
-                {
-                    dfs(i, j);
-                    island_count++;
-                }
-            }
-            //cout << endl;
         }
 
-        return island_count;
+        return true;
     }
 };
 
@@ -71,51 +58,9 @@ int main() {
 
     Solution s;
 
-
-    // Test case 1: Basic 4x5 grid with 2 islands
-    vector<vector<char>> grid1 = {
-        {'1', '1', '0', '0', '0'},
-        {'1', '1', '0', '0', '0'},
-        {'0', '0', '1', '0', '0'},
-        {'0', '0', '0', '1', '1'}
-    };
-    cout << "Number of islands in grid1: " << s.numIslands(grid1) << endl; // Expected: 3
-
-    // Test case 2: Grid with no land ('1')
-    vector<vector<char>> grid2 = {
-        {'0', '0', '0', '0', '0'},
-        {'0', '0', '0', '0', '0'},
-        {'0', '0', '0', '0', '0'}
-    };
-    cout << "Number of islands in grid2: " << s.numIslands(grid2) << endl; // Expected: 0
-
-    // Test case 3: Grid with all land ('1')
-    vector<vector<char>> grid3 = {
-        {'1', '1', '1'},
-        {'1', '1', '1'},
-        {'1', '1', '1'}
-    };
-    cout << "Number of islands in grid3: " << s.numIslands(grid3) << endl; // Expected: 1
-
-    // Test case 4: Grid with one vertical island
-    vector<vector<char>> grid4 = {
-        {'1', '0', '0'},
-        {'1', '0', '0'},
-        {'1', '0', '0'}
-    };
-    cout << "Number of islands in grid4: " << s.numIslands(grid4) << endl; // Expected: 1
-
-    // Test case 5: Grid with one horizontal island
-    vector<vector<char>> grid5 = {
-        {'0', '0', '0', '0'},
-        {'1', '1', '1', '1'},
-        {'0', '0', '0', '0'}
-    };
-    cout << "Number of islands in grid5: " << s.numIslands(grid5) << endl; // Expected: 1
-
-    // Test case 6: Empty grid (edge case)
-    vector<vector<char>> grid6 = {};
-    cout << "Number of islands in grid6: " << s.numIslands(grid6) << endl; // Expected: 0 (if handled)
+    // Test Case 1: Simple case with 4 nodes
+    vector<vector<int>> dislikes1 = {{1, 2}, {1, 3}, {1, 4}, {2, 3}};
+    cout << "Test Case 1 (Expected: true): " << (s.numPart(4, dislikes1) ? "true" : "false") << endl;
 
 
     return 0;

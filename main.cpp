4a5945aaae00f878e88222b037464decc1cf2eb5
aliza <alizarg21@gmail.com>
2025-05-06 14:29:50 -0700

    Completed

--- main.cpp
+++ main.cpp
@@ -27,300 +27,125 @@ struct TreeNode
     }
 };
 
-class Solution {
+class Solution
+{
 public:
-
-
-    vector<int> preOrder(TreeNode* root)
+    void islandsAndTreasure(vector<vector<int>>& grid)
     {
-        TreeNode* node;
-        stack<TreeNode*> stk;
-        vector<int> res;
+        if (grid.empty() || grid[0].empty()) return;
 
-        stk.push(root);
+        int inf = 2147483647;
+        vector<vector<bool>> visited(grid.size(), vector<bool>(grid[0].size(), false));
+        queue<pair<int, int>> q;
 
-        while (!stk.empty())
+
+        for (int i = 0; i < grid.size(); i++)
         {
-            TreeNode* node = stk.top();
-            stk.pop();
-            res.push_back(node->val);
-
-            if (node->right)
+            for (int j = 0; j < grid[i].size(); j++)
             {
-                stk.push(node->right);
-            }
-            if (node->left)
-            {
-                stk.push(node->left);
+                if (grid[i][j] == 0)
+                {
+                    q.emplace(i, j);
+                    visited[i][j] = true;
+                }
             }
         }
 
-        return res;
-    }
+        int dist = 0;
 
-
-
-    vector<int> bfs(TreeNode* root)
-    {
-        TreeNode* node;
-        queue<TreeNode*> q;
-        vector<int> res;
-
-        q.push(root);
+        vector<vector<int>> dir = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
 
         while (!q.empty())
         {
-            TreeNode* node = q.front();
-            q.pop();
-            res.push_back(node->val);
+            int n = q.size();
+            dist++;
 
-            if (node->left)
+            for (int i = 0; i < n; i++)
             {
-                q.push(node->left);
-            }
+                pair<int, int> node = q.front();
+                q.pop();
 
-            if (node->right)
-            {
-                q.push(node->right);
-            }
+                for (auto d : dir)
+                {
+                    int nr = d[0] + node.first;
+                    int nc = d[1] + node.second;
 
+                    if ((nr >= 0 && nr < grid.size()) &&
+                        (nc >= 0 && nc < grid[nr].size()))
+                    {
+                        if (grid[nr][nc] == inf &&
+                            grid[nr][nc] != -1 &&
+                            !visited[nr][nc])
+                        {
+                            grid[nr][nc] = dist;
+                            visited[nr][nc] = true;
+                            q.push({nr, nc});
+                        }
+                    }
+                }
+            }
         }
 
-        return res;
-    }
-
-
-
-    vector<int> inOrder(TreeNode* root)
-    {
-        TreeNode* node = root;
-        stack<TreeNode*> stk;
-        vector<int> res;
-
-        while (!stk.empty() || node)
+        // Print the grid
+        for (const auto& row : grid)
         {
-            
-            while (node)
+            for (const auto& cell : row)
             {
-                stk.push(node);
-                node = node->left;
+                cout << cell << " ";
             }
-
-            node = stk.top();
-            stk.pop();
-            res.push_back(node->val);
-
-            node = node->right;
-            
+            cout << endl;
         }
-        return res;
-    }
-
-    
-    vector<int> postOrder(TreeNode* root)
-    {
-        TreeNode* node;
-        stack<TreeNode*> stk;
-        stack<TreeNode*> stk2;
-        vector<int> res;
-
-        stk.push(root);
-
-        while (!stk.empty())
-        {
-            TreeNode* node = stk.top();
-            stk.pop();
-            stk2.push(node);
-
-            if (node->left)
-            {
-                stk.push(node->left);
-            }
-
-            if (node->right)
-            {
-                stk.push(node->right);
-            }
-
-        }
-
-        while (!stk2.empty())
-        {
-            res.push_back(stk2.top()->val);
-            stk2.pop();
-        }
-
-        return res;
-    }
-
-
-    int goodNodes(TreeNode* root) {
-        //This looks like a DFS function. We keep going down the nodes. We can do it iteratively. While going down, we
-        //keep the maximum val node we have found in that path (can use a stack). Use max() function to keep track of highest value in path.
-        //This is missing a part of the logic because we are doing pre-order traversal. But if we do Post order, then we can effectively see
-        //which children are larger.
-        
-        stack<TreeNode*> stk;
-        stack<TreeNode*> max_nodes;
-        int max = INT32_MIN;
-        int count = 0;
-        
-        stk.push(root);
-        max_nodes.push(root);
-        
-        while (!stk.empty())
-        {
-            TreeNode* node = stk.top();
-            
-            
-            if (!max_nodes.empty() && node->val >= max_nodes.top()->val)
-            {
-                count ++;
-                max_nodes.push(node);
-                //max = node->val;
-            }
-            
-            stk.pop();
-
-            if (node->right)
-            {
-                stk.push(node->right);
-            }
-            if (node->left)
-            {
-                stk.push(node->left);
-            }
-        }
-        return count;
     }
 };
 
 
-
-
-// Helper function to create a binary tree
-TreeNode* createTree(const vector<int>& values, int index = 0)
-{
-    if (index >= values.size() || values[index] == -1)
-    {
-        // -1 represents a null node
-        return nullptr;
-    }
-    auto* root = new TreeNode(values[index]);
-    root->left = createTree(values, 2 * index + 1);
-    root->right = createTree(values, 2 * index + 2);
-    return root;
-}
-
 // Main function provided by user - unchanged.
 int main()
 {
-
     Solution s;
 
+    // Example 1
+    vector<vector<int>> grid1 = {
+        {2147483647, -1, 0, 2147483647},
+        {2147483647, 2147483647, 2147483647, -1},
+        {2147483647, -1, 2147483647, -1},
+        {0, -1, 2147483647, 2147483647}
+    };
+    cout << "Example 1 Input:" << endl;
+    s.islandsAndTreasure(grid1);
 
-    // Create a binary tree:
-    //         5
-    //       /   \
-    //      3     7
-    //     / \   / \
-    //    2   4 6   8
-    vector<int> treeValues = {5, 3, 7, 2, 4, 6, 8};
-    TreeNode* root = createTree(treeValues);
+    // Example 2
+    vector<vector<int>> grid2 = {
+        {0, -1},
+        {2147483647, 2147483647}
+    };
+    cout << "\nExample 2 Input:" << endl;
+    s.islandsAndTreasure(grid2);
 
-    // Expected results
-    vector<int> expectedPreOrder = {5, 3, 2, 4, 7, 6, 8};
-    vector<int> expectedBFS = {5, 3, 7, 2, 4, 6, 8};
-    vector<int> expectedInOrder = {2, 3, 4, 5, 6, 7, 8};
+    // Example 3: Multiple treasures
+    vector<vector<int>> grid3 = {
+        {0, 2147483647, 0},
+        {2147483647, -1, 2147483647},
+        {0, 2147483647, 2147483647}
+    };
+    cout << "\nExample 3 Input:" << endl;
+    s.islandsAndTreasure(grid3);
 
-    // Call preOrder
-    vector<int> result = s.preOrder(root);
 
-    // Compare and print
-    cout << "preOrder traversal: [";
-    for (size_t i = 0; i < result.size(); ++i)
-    {
-        cout << result[i];
-        if (i < result.size() - 1) cout << ", ";
-    }
-    cout << "]" << endl;
+    // Example 4: Single cell
+    vector<vector<int>> grid4 = {{0}};
+    cout << "\nExample 4 Input:" << endl;
+    s.islandsAndTreasure(grid4);
 
-    cout << "Expected: [";
-    for (size_t i = 0; i < expectedPreOrder.size(); ++i)
-    {
-        cout << expectedPreOrder[i];
-        if (i < expectedPreOrder.size() - 1) cout << ", ";
-    }
-    cout << "]" << endl;
 
-    cout << endl;
-
-    // Call bfs
-    result = s.bfs(root);
-
-    // Compare and print
-    cout << "BFS traversal: [";
-    for (size_t i = 0; i < result.size(); ++i)
-    {
-        cout << result[i];
-        if (i < result.size() - 1) cout << ", ";
-    }
-    cout << "]" << endl;
-
-    cout << "Expected: [";
-    for (size_t i = 0; i < expectedBFS.size(); ++i)
-    {
-        cout << expectedBFS[i];
-        if (i < expectedBFS.size() - 1) cout << ", ";
-    }
-    cout << "]" << endl;
-
-    cout << endl;
-
-    // Call inOrder
-    result = s.inOrder(root);
-
-    // Compare and print
-    cout << "inOrder traversal: [";
-    for (size_t i = 0; i < result.size(); ++i)
-    {
-        cout << result[i];
-        if (i < result.size() - 1) cout << ", ";
-    }
-    cout << "]" << endl;
-
-    cout << "Expected: [";
-    for (size_t i = 0; i < expectedInOrder.size(); ++i)
-    {
-        cout << expectedInOrder[i];
-        if (i < expectedInOrder.size() - 1) cout << ", ";
-    }
-    cout << "]" << endl;
-
-    cout << endl;
-
-    // Call postOrder
-    result = s.postOrder(root);
-
-    // Compare and print
-    cout << "postOrder traversal: [";
-    for (size_t i = 0; i < result.size(); ++i)
-    {
-        cout << result[i];
-        if (i < result.size() - 1) cout << ", ";
-    }
-    cout << "]" << endl;
-
-    // Expected post-order traversal
-    vector<int> expectedPostOrder = {2, 4, 3, 6, 8, 7, 5};
-
-    cout << "Expected: [";
-    for (size_t i = 0; i < expectedPostOrder.size(); ++i)
-    {
-        cout << expectedPostOrder[i];
-        if (i < expectedPostOrder.size() - 1) cout << ", ";
-    }
-    cout << "]" << endl;
+    // Example 5: All walls
+    vector<vector<int>> grid5 = {
+        {0, 2147483647, 2147483647, -1, 2147483647},
+        {2147483647, 2147483647, 2147483647, -1, 2147483647},
+        {2147483647, 2147483647, 2147483647, -1, 2147483647}
+    };
+    cout << "\nExample 5 Input:" << endl;
+    s.islandsAndTreasure(grid5);
 
     return 0;
 }

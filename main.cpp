01e930fb4a782b4e0c8a508a3d8ad5b4a4d23281
aliza <alizarg21@gmail.com>
2025-05-08 22:57:07 -0700

    Completed

--- main.cpp
+++ main.cpp
@@ -6,11 +6,11 @@
 
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-
-    int rob(vector<int>& nums) {
-
+    int rob(vector<int>& nums)
+    {
         if (nums.size() == 1)
         {
             return nums[0];
@@ -21,15 +21,15 @@ public:
             return max(nums[0], nums[1]);
         }
 
-        vector<int> dp (nums.size());
+        vector<int> dp(nums.size());
         int res;
 
         dp[0] = nums[0];
-        dp[1]= max(dp[0], nums[1]);
+        dp[1] = max(dp[0], nums[1]);
 
         for (int i = 2; i < nums.size(); i++)
         {
-            dp[i] = max(dp[i-1], nums[i] + dp[i-2]);
+            dp[i] = max(dp[i - 1], nums[i] + dp[i - 2]);
             res = dp[i];
         }
 
@@ -37,6 +37,32 @@ public:
         return res;
     }
 
+
+    int rob2(vector<int>& nums)
+    {
+        if (nums.size() == 1)
+        {
+            return nums[0];
+        }
+
+        auto solveDp = [&](vector<int> numtemp)
+        {
+            vector<int> dp(numtemp.size());
+            //int res;
+
+            dp[0] = numtemp[0];
+            dp[1] = max(dp[0], numtemp[1]);
+
+            for (int i = 2; i < numtemp.size(); i++)
+            {
+                dp[i] = max(dp[i - 1], numtemp[i] + dp[i - 2]);
+                //res = dp[i];
+            }
+            return dp.back();
+        };
+
+        return max(solveDp(vector<int>(nums.begin(), nums.end()-1)), solveDp(vector<int>(nums.begin()+1, nums.end())));
+    }
 };
 
 
@@ -45,8 +71,8 @@ int main()
 {
     Solution s;
 
-    vector<int> test1 = {1, 3, 1};
-    cout << "Test 1: Expected: 3, Actual: " << s.rob(test1) << endl;
+    vector<int> test1 = {3, 4, 3};
+    cout << "Test 1: Expected: 3, Actual: " << s.rob2(test1) << endl;
 
     vector<int> test2 = {2, 7, 9, 3, 1};
     cout << "Test 2: Expected: 12, Actual: " << s.rob(test2) << endl;

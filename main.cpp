458e52d749e5e0256b5d238ebf55eee9fb702fd4
aliza <alizarg21@gmail.com>
2025-05-09 17:36:44 -0700

    Completed

--- main.cpp
+++ main.cpp
@@ -1,77 +1,58 @@
 #include <iostream>
+#include <unordered_map>
+#include <set>
+#include <stack>
 #include <vector>
 
 using namespace std;
 
 class Solution {
 public:
-    string longestPalindrome(string s) {
-        //'a b a b d b a'
+    int numDecodings(string s) {
+        //what we really want to find is a form of all(some) possible combos, where order matters.
+        set<int> dp;
+        vector<string> v;
 
-        if (s.size() == 0)
+
+        struct Frame
         {
-            return "";
-        }
-        vector<vector<bool>> dp(s.size(), vector<bool>(s.size(), false));
-        string res = "";
-        int resLen = 0;
-        int resIndex = -1;
+            int start;
+            int end;
+            string comb;
+        };
 
-        for (int i = 0; i < s.size(); i++)
+        stack<Frame> stk;
+        stk.push({0, 0, ""});
+
+        while (!stk.empty())
         {
-            for (int j = 0; j <= i; j++)
-            {
-                if (s[j] == s[i]) { // Outer characters match
-                    if (i - j < 2) { // Length 1 (i==j) or Length 2 (i==j+1)
-                        dp[i][j] = true;
-                    } else { // Length 3 or more
-                        dp[i][j] = dp[i-1][j+1]; // Check inner palindrome s[j+1...i-1]
-                    }
+            Frame node = stk.top();
+            stk.pop();
+            int strt = node.start;
+            int end = node.end;
+            string comb = node.comb;
 
-                    if (dp[i][j] && (i - j + 1 > resLen)) {
-                        resLen = i - j + 1;
-                        resIndex = j;
-                    }
-                }
-            }
+            if (strt > end && comb.size() == s.size())
+                continue;
+
+            v.push_back(comb);
+
+            if (strt == end )
+                stk.push({strt, end+1, comb+s[strt]});
+            else
+                stk.push({strt+1, end, comb+s[strt]});
+            
+            
         }
 
-        return s.substr(resIndex, resLen);
 
+        // Print vector v
+        for (const auto& str : v)
+        {
+            cout << str << ", " << endl;
+        }
+        return v.size();
     }
-
-
-    int countSubstrings(string s) {
-        //'a b a b d b a'
-        vector<vector<bool>> dp(s.size(), vector<bool>(s.size(), false));
-        int count = 0;
-
-        for (int i = 0; i < s.size(); i++)
-        {
-            for (int j = 0; j <= i; j++)
-            {
-                if (s[j] == s[i])
-                {
-                    if (i - j < 2)
-                    {
-                        dp[j][i] = true;
-                    } else
-                    {
-                        dp[j][i] = dp[j+1][i-1];
-                    }
-
-                    if (dp[j][i])
-                    {
-                        count++;
-                    }
-                }
-            }
-        }
-
-        return count;
-
-    }
-
 };
 
 
@@ -80,42 +61,7 @@ int main()
 {
     Solution s;
 
-    // Test cases
-    vector<pair<string, string>> testCases = {
-        {"babad", "bab"},
-        {"cbbd", "bb"},
-        {"a", "a"},
-        {"ac", "a"},
-        {"abba", "abba"},
-        {"aaaaa", "aaaaa"},
-        {"", ""}
-    };
-
-    for (const auto& test : testCases)
-    {
-        string result = s.longestPalindrome(test.first);
-        cout << "Input: \"" << test.first << "\"\n";
-        cout << "Expected: \"" << test.second << "\"\n";
-        cout << "Output: \"" << result << "\"\n";
-        cout << "Result: " << (result == test.second ? "PASS" : "FAIL") << "\n\n";
-    }
-
-    vector<pair<string, int>> countTestCases = {
-        {"abc", 3},
-        {"aaa", 6},
-        {"abba", 6},
-        {"", 0}
-    };
-
-    cout << "\nTesting countSubstrings:\n";
-    for (const auto& test : countTestCases)
-    {
-        int result = s.countSubstrings(test.first);
-        cout << "Input: \"" << test.first << "\"\n";
-        cout << "Expected: " << test.second << "\n";
-        cout << "Output: " << result << "\n";
-        cout << "Result: " << (result == test.second ? "PASS" : "FAIL") << "\n\n";
-    }
+    s.numDecodings("1023");
 
     return 0;
 }

05904742505b437ecc7a016aac14f2ba358ac950
aliza <alizarg21@gmail.com>
2025-05-15 00:40:44 -0700

    Completed

--- main.cpp
+++ main.cpp
@@ -3,103 +3,125 @@
 
 using namespace std;
 
-class Solution
-{
+class Solution {
 public:
-    void rotate(vector<vector<int>>& matrix)
-    {
-        vector<vector<int>> res(matrix.size(), vector<int>(matrix[0].size(), -1));
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        int visited = 0;
+        int limit = matrix.size()*matrix[0].size();
 
-        for (int i = 0; i < matrix.size(); i++)
-        {
-            for (int j = 0; j< matrix[i].size(); j++)
-            {
-                int newCol = matrix[i].size() - i - 1;
-                res[j][newCol] = matrix[i][j];
-            }
+        bool upturn, downturn, leftturn, rightturn;
+        upturn = false; downturn = false; leftturn = false; rightturn = true;
+
+        int up, down, left, right;
+        up = 0;
+        down = matrix.size();
+        left = 0;
+        right = matrix[0].size();
+
+        pair<int, int> current = {0, 0};
+
+        vector<int> res;
+
+
+        if (matrix[0].size() == 1) {
+            vector<int> out;
+            for (auto &row : matrix) out.push_back(row[0]);
+            return out;
         }
 
-        matrix = res;
+
+        while (visited < limit)
+        {
+
+            res.push_back(matrix[current.first][current.second]);
+
+            if (current.second < right-1 && rightturn)
+            {
+                current.second++;
+
+                if (current.second == right-1)
+                {
+                    right--;
+                    rightturn = false;
+                    downturn = true;
+                }
+
+
+            }else
+
+            if (current.first < down-1 && downturn)
+            {
+                current.first++;
+
+                if (current.first == down - 1)
+                {
+                    down--;
+                    downturn = false;
+                    leftturn = true;
+                }
+            } else
+
+            if (current.second > left && leftturn)
+            {
+                current.second--;
+
+                if (current.second == left)
+                {
+                    left++;
+                    leftturn = false;
+                    upturn = true;
+                }
+            } else
+
+            if (current.first > up && upturn)
+            {
+                current.first--;
+
+                if (current.first == up + 1)
+                {
+                    up++;
+                    upturn = false;
+                    rightturn = true;
+                }
+            }
+
+
+            visited++;
+        }
+        return res;
     }
 };
+
 
 
 int main()
 {
     Solution s;
 
-// Test case 1: 3x3 matrix
-    vector<vector<int>> matrix1 = {
-        {1, 2, 3},
-        {4, 5, 6},
-        {7, 8, 9}
-    };
-    vector<vector<int>> expected1 = {
-        {7, 4, 1},
-        {8, 5, 2},
-        {9, 6, 3}
-    };
-    s.rotate(matrix1);
-    cout << "Test case 1:\nExpected:\n";
-    for (const auto& row : expected1)
-    {
-        for (const auto& val : row) cout << val << " ";
-        cout << "\n";
-    }
-    cout << "Actual:\n";
-    for (const auto& row : matrix1)
-    {
-        for (const auto& val : row) cout << val << " ";
-        cout << "\n";
-    }
+    vector<vector<int>> matrix1 = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    vector<vector<int>> matrix2 = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
 
-    // Test case 2: 4x4 matrix
-    vector<vector<int>> matrix2 = {
-        {5, 1, 9, 11},
-        {2, 4, 8, 10},
-        {13, 3, 6, 7},
-        {15, 14, 12, 16}
-    };
-    vector<vector<int>> expected2 = {
-        {15, 13, 2, 5},
-        {14, 3, 4, 1},
-        {12, 6, 8, 9},
-        {16, 7, 10, 11}
-    };
-    s.rotate(matrix2);
-    cout << "\nTest case 2:\nExpected:\n";
-    for (const auto& row : expected2)
+    cout << "Test case 1:" << endl;
+    vector<int> result1 = s.spiralOrder(matrix1);
+    cout << "Expected: [1,2,3,6,9,8,7,4,5]" << endl;
+    cout << "Got: [";
+    for (int i = 0; i < result1.size(); i++)
     {
-        for (const auto& val : row) cout << val << " ";
-        cout << "\n";
+        cout << result1[i];
+        if (i < result1.size() - 1) cout << ",";
     }
-    cout << "Actual:\n";
-    for (const auto& row : matrix2)
-    {
-        for (const auto& val : row) cout << val << " ";
-        cout << "\n";
-    }
+    cout << "]" << endl;
 
-    // Test case 3: Empty matrix
-    vector<vector<int>> matrix3;
-    s.rotate(matrix3);
-    cout << "\nTest case 3:\nEmpty matrix - no output expected\n";
+    cout << "\nTest case 2:" << endl;
+    vector<int> result2 = s.spiralOrder(matrix2);
+    cout << "Expected: [1,2,3,4,8,12,11,10,9,5,6,7]" << endl;
+    cout << "Got: [";
+    for (int i = 0; i < result2.size(); i++)
+    {
+        cout << result2[i];
+        if (i < result2.size() - 1) cout << ",";
+    }
+    cout << "]" << endl;
 
-    // Test case 4: 1x1 matrix
-    vector<vector<int>> matrix4 = {{1}};
-    vector<vector<int>> expected4 = {{1}};
-    s.rotate(matrix4);
-    cout << "\nTest case 4:\nExpected:\n";
-    for (const auto& row : expected4)
-    {
-        for (const auto& val : row) cout << val << " ";
-        cout << "\n";
-    }
-    cout << "Actual:\n";
-    for (const auto& row : matrix4)
-    {
-        for (const auto& val : row) cout << val << " ";
-        cout << "\n";
-    }
     return 0;
 }
